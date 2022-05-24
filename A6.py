54826253a7df5257b1f92d62399129f2e7d5a6a8
Anubhav1105 <104314385+Anubhav1105@users.noreply.github.com>
2022-05-24 22:07:55 +0530

    Update A6.py

--- A6.py
+++ A6.py
@@ -1,11 +1,7 @@
-# Get the value of a and b
-a = int (input("Enter the Value of a: "))
-b = int(input("Enter the Value of b: "))
-# Calculating xor of a and b
-num = a ^ b
-Count_flipped_bit = 0
-# Counting Number of set bit present
-while (num != 0):
-    num = num & (num - 1)
-    Count_flipped_bit += 1
-print("Number of bits needed to be flipped to convert a to b is:", Count_flipped_bit)
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+
+# We can do this by calculating Xor of a and b, then converting the result to a binary form.
+c = bin(a^b)
+
+print("Number of bits that need to be flipped to convert 'a' to 'b' =", c.count('1'))
